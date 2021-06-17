8a484aa387a8b3d9df1171f828f2c40021ce811c
codanv <codanv@codanv.com>
2021-06-17 09:03:11 +0530

    testing by deploying react app to github

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Demo = () => {
+    return (
+        <>
+        <h1>Demonstration</h1>
+        <p>Yes. you have done this.</p>
+        </>
+    )
+}
+
+ReactDOM.render(<Demo />, document.getElementById("root"))
