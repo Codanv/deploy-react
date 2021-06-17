3d0c6315242b4a68667a67c7911a2b87368264f8
codanv <codanv@codanv.com>
2021-06-17 09:10:38 +0530

    Updates

---
+++ static/js/main.d5f8f9d3.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpdeploy-react"]=this["webpackJsonpdeploy-react"]||[]).push([[0],{4:function(e,t,n){"use strict";n.r(t);n(1);var c=n(3),r=n.n(c),s=n(0),o=function(){return Object(s.jsxs)(s.Fragment,{children:[Object(s.jsx)("h1",{children:"Demonstration"}),Object(s.jsx)("p",{children:"Yes. you have done this."})]})};r.a.render(Object(s.jsx)(o,{}),document.getElementById("root"))}},[[4,1,2]]]);
+//# sourceMappingURL=main.d5f8f9d3.chunk.js.map
